417bc52ca39599d23487ba682296ed7ba7e82472
JeffMuter <muterjeffery@gmail.com>
2023-03-23 21:26:36 -0400

    header png

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,11 @@
 
 const About = (props) => {
   return (
-    <section id="about" className="h-screen">
+    <section
+      id="about"
+      className="h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url(/background-home.jpg)" }}
+    >
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font font-size: 9rem line-height: 1 mb-4 font-medium text-white">
