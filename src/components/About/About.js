5b4d63e69bed1798712d86e3b1832ea86fabe8ab
JeffMuter <muterjeffery@gmail.com>
2023-03-27 14:48:06 -0400

    color trouble,, changed about styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,13 +9,13 @@ const About = (props) => {
       style={{ backgroundImage: "url(/background-home.jpg)" }}
     >
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font font-size: 9rem line-height: 1 mb-4 font-medium text-white">
+        <div className="lg:pr-24 mr- flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center w-1/3">
+          <p className="title-font text-5xl mb-4 font-medium text-red-400 !important">
             Hello There,
-          </h1>
-          <h1 className="title-font font-size: 9rem line-height: 1 mb-4 font-medium text-white">
+          </p>
+          <p className="title-font text-5xl mb-4 font-medium text-white">
             My name is Jeff Muter, a self-taught front end web developer.
-          </h1>
+          </p>
         </div>
       </div>
     </section>
