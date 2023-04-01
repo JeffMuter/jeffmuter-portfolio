bb74898c2847e3f2b06b09e3fc5e8774c37073a9
JeffMuter <muterjeffery@gmail.com>
2023-03-31 23:00:50 -0400

    unfixed nav, unncecessary

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import React from "react";
 const NavBar = (props) => {
   const onClickHandler = (event) => {};
   return (
-    <div className="overflow-hidden float-right bg-transparent list-none m-0 fixed top-0 w-full z-100 text-3xl font-custom">
+    <div className="overflow-hidden float-right bg-transparent list-none m-0 top-0 w-full z-100 text-3xl font-custom">
       <ul>
         <li className="text-teal float-right mr-4 font-medium">
           <h3 class="font-custom">Projects</h3>
