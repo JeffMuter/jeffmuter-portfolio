9a6129ae9c76499bca0b39a2083742bebdd2fabc
JeffMuter <muterjeffery@gmail.com>
2023-04-01 20:45:20 -0400

    fixed email function

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,32 +1,9 @@
 // src/components/Contact.js
-import { useRef } from "react";
+import Email from "../Email/Email";
 import React from "react";
-import emailjs, { sendForm } from "emailjs-com";
 
 const Contact = (props) => {
   //Users can send me an email after giving some basic info via a form that takes up the right side of the bottom section of the page.On the bottom left is the Google Maps API zoomed into my area.
-
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    //all the required info in order for the info to get to EmailJS & indentify my account.
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
-        form.current,
-        process.env.REACT_APP_USER_ID
-      )
-      //.then used because sendForm is asynchronous.
-      //if clear, logs the text, if error, error message
-      .then(
-        (result) => console.log(result.text),
-        (error) => console.log(error.text)
-      );
-    e.target.reset();
-  };
 
   function encode(data) {
     return Object.keys(data)
@@ -75,46 +52,7 @@ const Contact = (props) => {
           </div>
         </div>
         {/* contains the form that will take details and send a personal email to me */}
-        <form
-          ref={form}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-          onSubmit={sendEmail}
-        >
-          <label className="text-teal sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Let's Work Together
-          </label>
-          <div className="relative mb-4">
-            <label className="leading-7 text-sm text-teal">Name</label>
-            <input
-              type="text"
-              name="user_name"
-              required
-              className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label className="leading-7 text-sm text-teal">Email</label>
-            <input
-              type="email"
-              name="user_email"
-              required
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label className="leading-7 text-sm text-teal">Message</label>
-            <textarea
-              name="message"
-              required
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <input
-            type="submit"
-            value="Submit"
-            className="text-teal bg-pink border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          ></input>
-        </form>
+        <Email />
       </div>
     </section>
   );
