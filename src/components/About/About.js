43464e07b4972a645d452fd1ae509d1e5a9be091
JeffMuter <muterjeffery@gmail.com>
2023-06-12 19:05:52 -0400

    gradient to image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -30,25 +30,30 @@ const About = (props) => {
   //changes the background for mobile because the background image makes it too difficult to read.
   const style = isMobile
     ? { backgroundColor: "#191919" }
-    : { backgroundImage: "url(/background-home.jpg)" };
-
+    : {
+        backgroundImage: `linear-gradient(to bottom, rgba(25, 25, 25, 0) 85%, rgba(25, 25, 25, 1)), url(/background-home.jpg)`,
+      };
   return (
-    <section id="about" className="h-screen bg-cover bg-center" style={style}>
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:pr-24 mr-auto flex flex-col items-start text-left font-gothicCentury mb-16 md:mb-0  w-1/2">
-          <p className="title-font text-4xl md:text-5xl lg:text-7xl font-medium text-teal">
-            Hello,
-          </p>
-          <p
-            className={`inline-block transform transition-transform duration-1 ease-in-out ${
-              isLoaded ? 'translate-x-0' : '-translate-x-full'
-            } delay-500 title-font text-3xl md:lg:text-5xl leading-tight mb-4 mt-7 font-medium text-teal`}
-          >
-            My name is Jeff Muter, <br/> Software Developer, <br/> New Father, <br/> Planeswalker, <br/> Innovation Lover, <br/> Lifelong Learner
-          </p>
+    <div className="relative bg-purple">
+      <section id="about" className="h-screen bg-cover bg-center" style={style}>
+        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+          <div className="lg:pr-24 mr-auto flex flex-col items-start text-left font-gothicCentury mb-16 md:mb-0  w-1/2">
+            <p className="title-font text-4xl md:text-5xl lg:text-7xl font-medium text-teal">
+              Hello,
+            </p>
+            <p
+              className={`inline-block transform transition-transform duration-1 ease-in-out ${
+                isLoaded ? "translate-x-0" : "-translate-x-full"
+              } delay-500 title-font text-3xl md:lg:text-5xl leading-tight mb-4 mt-7 font-medium text-teal`}
+            >
+              My name is Jeff Muter, <br /> Software Developer, <br /> New
+              Father, <br /> Planeswalker, <br /> Innovation Lover, <br />{" "}
+              Lifelong Learner
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 export default About;
