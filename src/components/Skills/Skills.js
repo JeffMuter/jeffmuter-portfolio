74bd81e20c596fc1f2baafbd7d0cfaa7404b3556
JeffMuter <muterjeffery@gmail.com>
2023-06-18 21:49:47 -0400

    box-shadow function and application to skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { BadgeCheckIcon, ChipIcon, ChevronDownIcon } from "@heroicons/react/solid";
+import {
+  BadgeCheckIcon,
+  ChipIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/solid";
 import { skills } from "../data";
 
 const Skills = () => {
@@ -15,13 +19,16 @@ const Skills = () => {
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className="bg-pink rounded flex relative">
+              <div className="bg-pink rounded flex relative transition-shadow duration-300 hover:box-shadow-teal">
                 <BadgeCheckIcon className="text-teal fill-teal w-6 h-6 flex-shrink-0 m-4 mr-4" />
                 <span className="title-font font-medium text-teal mt-4">
                   {skill}
                 </span>
                 <div className="ml-auto w-12 h-full">
-                  <div id="child" className="w-6 h-6 ease-in scale-100 hover:scale-125 duration-200 m-4">
+                  <div
+                    id="child"
+                    className="w-6 h-6 ease-in scale-100 hover:scale-125 duration-200 m-4"
+                  >
                     <div className="rounded-full transition ease-out duration-200 hover:border-teal hover:border-2">
                       <ChevronDownIcon className="text-teal fill-teal w-4.5" />
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
-// @type {import('tailwindcss').Config} 
-const plugin = require('tailwindcss/plugin')
-
+// @type {import('tailwindcss').Config}
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./src/components/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src/components/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
-
-      textShadow: { // apply with text-shadow*add color name*
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-        teal: '0 0 14px rgba(184, 185, 237, 0.2), 0 0 15px rgba(184, 185, 237, 0.2), 0 0 12px rgba(184, 185, 237, 0.2), 0 0 8px rgba(184, 185, 237, 0.2), 0 0 18px rgba(184, 185, 237, 0.2), 0 0 17px rgba(184, 185, 237, 0.2), 0 0 17px rgba(184, 185, 237, 0.2), 0 0 17px rgba(184, 185, 237, 0.2)',
+      textShadow: {
+        // apply with text-shadow*add color name*
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+        teal: "0 0 14px rgba(184, 185, 237, 0.2), 0 0 15px rgba(184, 185, 237, 0.2), 0 0 12px rgba(184, 185, 237, 0.2), 0 0 8px rgba(184, 185, 237, 0.2), 0 0 18px rgba(184, 185, 237, 0.2), 0 0 17px rgba(184, 185, 237, 0.2), 0 0 17px rgba(184, 185, 237, 0.2), 0 0 17px rgba(184, 185, 237, 0.2)",
       },
+
+      boxShadow: {
+        teal: "0px 0px 14px 8px rgba(184, 185, 237, 0.5)",
+      },
+
       fontFamily: {
         customFont: ["Varela Round", "sans - serif"],
       },
@@ -31,12 +37,24 @@ module.exports = {
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
         {
-          'text-shadow': (value) => ({
+          "box-shadow": (value) => ({
+            boxShadow: value,
+          }),
+        },
+        {
+          values: theme("boxShadow"),
+        }
+      );
+    }),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
             textShadow: value,
           }),
         },
-        { values: theme('textShadow') }
-      )
+        { values: theme("textShadow") }
+      );
     }),
   ],
 };
