36d2ab0722e7e6a9b1f795f2231ea81d502c53cd
Jeff Muter <muterjeffery@gmail.com>
2023-06-21 06:25:49 -0400

    opacity email, coral touch

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -49,22 +49,25 @@ const Email = (props) => {
           type="text"
           name="user_name"
           required
-          className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        />
+          className="w-full rounded border border-teal focus:border-coral focus:border-2 text-coral py-1 px-3 leading-8"
+          style={{ backgroundColor: "transparent" }}
+          />
         {/* input field for user's email */}
         <label className="leading-7 text-sm text-teal">Email</label>
         <input
           type="email"
           name="user_email"
           required
-          className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none  py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          className="w-full rounded border border-teal focus:border-coral focus:border-2 text-coral py-1 px-3 leading-6"
+          style={{ backgroundColor: "transparent" }}
         />
         {/* input field for message */}
         <label className="leading-7 text-sm text-teal">Message</label>
         <textarea
           name="message"
           required
-          className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+          className="w-full rounded border border-teal focus:border-coral focus:border-2 h-32 text-base text-coral py-1 px-3 resize-none leading-6"
+          style={{ backgroundColor: "transparent" }}
         />
         {/* button to submit the form */}
         <button
