f1bb8c7c6a82b6f6a09e503ab3a6a5114afb7d59
Jeff Muter <muterjeffery@gmail.com>
2023-07-24 18:48:50 -0400

    Skills create individual skill

---
+++ src/components/Skills/Skill.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import {
+    BadgeCheckIcon,
+    ChevronDownIcon,
+  } from "@heroicons/react/solid";
+
+const Skill =({ skill })=>{
+    const [isHovering, setIsHovering] = useState(false);
+
+    const handleMouseEnter = () => {setIsHovering(true)};
+    const handleMouseLeave = () => {setIsHovering(false)};
+    const expanded = isHovering 
+    ? "transform transition-shadow duration-300 hover:box-shadow-teal hover:scale-y-2"
+     : " h-16"
+    return (
+        <div key={skill.id} className={`p-2 sm:w-1/2 w-full`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className={`bg-pink ${expanded} rounded flex relative`} >
+          <BadgeCheckIcon className="text-teal fill-teal w-6 h-6 flex-shrink-0 m-4 mr-4" />
+          <span className="title-font font-medium text-teal mt-4">
+            {skill.title}
+          </span>
+          <div className="ml-auto w-12 h-full">
+            <div
+              id="child"
+              className="w-6 h-6 ease-in scale-100 hover:scale-125 duration-200 m-4"
+            >
+              <div className="rounded-full transition ease-out duration-200 hover:border-teal hover:border-2">
+                <ChevronDownIcon className="text-teal fill-teal w-4.5" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    )
+}
+export default Skill;
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,26 +1,11 @@
-// import { useState, useReducer } from "react";
 import { skills } from "../data";
+import Skill from "./Skill";
 import {
-  BadgeCheckIcon,
   ChipIcon,
-  ChevronDownIcon,
 } from "@heroicons/react/solid";
-
 
 const Skills = () => {
 
-
-  // reducer function initial. Updates mapped div states.
-// const reducer = (state, action) => {
-//   let expanded = false;
-  
-//   return expanded;
-// }
-
-  // I want the skill divs to expand onClick and collapse onClick & off of the element
-  // const expandSkillOnClick = (skill) => {
-  //   setOpen(!open)
-  // }
   return (
     <section id="skills">
       <div className="container px-5 py-10 mx-auto">
@@ -30,31 +15,10 @@ const Skills = () => {
             Technical Skills
           </h1>
         </div>
-{/* onClick={expandSkillOnClick} */}
          <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2" > 
-          {skills.map((skills) => (
-            <div key={skills} className="p-2 sm:w-1/2 w-full" >
-              <div className="bg-pink rounded flex relative transition-shadow duration-300 hover:box-shadow-teal focus:h-32" >
-                <BadgeCheckIcon className="text-teal fill-teal w-6 h-6 flex-shrink-0 m-4 mr-4" />
-                <span className="title-font font-medium text-teal mt-4">
-                  {skills.title}
-                </span>
-                <div className="ml-auto w-12 h-full">
-                  <div
-                    id="child"
-                    className="w-6 h-6 ease-in scale-100 hover:scale-125 duration-200 m-4"
-                  >
-                    <div className="rounded-full transition ease-out duration-200 hover:border-teal hover:border-2">
-                      <ChevronDownIcon className="text-teal fill-teal w-4.5" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* <div>
-              { open && <div> <p className="bg-pink text-teal">{skill.description}</p></div> }
-              </div> */}
-            </div>
-          ))}
+           {skills.map((skill) => (
+             <Skill skill={skill} />
+           ))}
         </div>
       </div>
     </section>
