71cedef8e86f509ed74989759e56885ca3bd3813
JeffMuter <muterjeffery@gmail.com>
2023-08-02 23:31:45 -0400

    slideshow header

--- src/components/Projects/ProjectSlideShow.js
+++ src/components/Projects/ProjectSlideShow.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { projects } from "../data";
+import { CodeIcon } from "@heroicons/react/solid";
 
 const ProjectSlideShow = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNextSlide = () => {
-      console.log("click event is working");
+    console.log("click event is working");
     setCurrentIndex((prevIndex) => {
       if (prevIndex === projects.length - 1) {
         return 0;
@@ -18,13 +19,25 @@ const ProjectSlideShow = () => {
   let slide = projects[currentIndex];
 
   return (
-    <section id="slideshow" className="w-full">
-        <div className="m-10">
-            <h1 className="text-white">{slide.title}</h1>
-            <button type="button" className="bg-white relative z-50 cursor-pointer hover:bg-purple" onClick={handleNextSlide}>
-                Next Slide
-            </button>
+    <section id="slideshow" className="w-full h-[100vh]">
+      <div className="m-10">
+        <div className="flex flex-col w-full wb-20">
+          <CodeIcon className="mx-auto inline-block w-10 wb-4 fill-teal" />
+          <h1 className="sm:text-4xl text-3xl mx-auto font-medium title-font text-teal mb-8">
+            Apps I Have Built
+          </h1>
         </div>
+        <div>
+          <h1 className="text-white">{slide.title}</h1>
+          <button
+            type="button"
+            className="bg-white relative z-50 cursor-pointer hover:bg-purple"
+            onClick={handleNextSlide}
+          >
+            Next Slide
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
