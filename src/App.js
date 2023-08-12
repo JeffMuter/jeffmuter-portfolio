7e75827cb47d1ba34a2e2983bc834899daf690a4
Jeff Muter <muterjeffery@gmail.com>
2023-08-12 01:30:38 -0400

    slideshow near ready for browser

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Background from "./components/Background/Background";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import NavBar from "./components/NavBar/NavBar";
-import Projects from "./components/Projects/Projects";
+// import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 import ProjectSlideShow from "./components/Projects/ProjectSlideShow";
 import "tailwindcss/tailwind.css";
@@ -14,7 +14,7 @@ function App() {
           <Background />
           <About className="flex items-center" />
           <Skills />
-          <Projects />
+          {/* <Projects /> */}
           <ProjectSlideShow />
           <Contact />
         </main>
--- src/components/Projects/ProjectSlideShow.js
+++ src/components/Projects/ProjectSlideShow.js
@@ -1,4 +1,4 @@
-import { CodeIcon, ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 import { projects } from "../data";
 import "./Slideshow.css";
@@ -19,28 +19,22 @@ const ProjectSlideShow = () => {
   console.log("render");
   let slide = projects[currentIndex];
 
-  //TODO add grid layout to this component to line the content up nicely
+  //TODO add dynamic styling and effects
   return (
-    <section id="slideshow" className="flex flex-col items-center justify-center w-full h-auto mt-20">
-      <header className="text-center mb-8">
-        <CodeIcon className="w-10 h-10 fill-teal mx-auto" />
-        <h1 className="sm:text-4xl text-3xl font-medium title-font text-teal">
-          Projects and References
-        </h1>
-      </header>
+    <section id="slideshow" className="flex flex-col items-center justify-center w-full h-[100vh]">
       <section className="flex h-[80vh] w-[100vw] z-2">
-        <ChevronLeftIcon className="relative h-[10vw] w-[15vw] mx-[3vw] fill-teal self-center" onClick={handleNextSlide} />
-        <div class="grid-container">
-          <div class="filler1"></div>
-          <p class="slide-description" >{slide.description}</p>
-          <p class="slide-title">{slide.title}</p>
-          <div class="filler2"></div>
-          <button class="slide-code-button">Website</button>
-          <button class="slide-site-button">Code</button>
-          <div class="filler3"></div>
-          <p class="slide-tools"  dangerouslySetInnerHTML={{ __html: slide.subtitle }}></p>
+        <ChevronLeftIcon className="relative h-[10vw] w-[15vw] fill-teal self-center" onClick={handleNextSlide} />
+        <div class="grid-container" style={{ backgroundImage: `url(${slide.image})` }}>
+          <div class="filler1 appear-when-hover"></div>
+          <p class="slide-description appear-when-hover" >{slide.description}</p>
+          <p class="slide-title appear-when-hover">{slide.title}</p>
+          <div class="filler2 appear-when-hover"></div>
+          <button class="slide-code-button appear-when-hover">Website</button>
+          <button class="slide-site-button appear-when-hover">Code</button>
+          <div class="filler3 appear-when-hover"></div>
+          <p class="slide-tools appear-when-hover"  dangerouslySetInnerHTML={{ __html: slide.subtitle }}></p>
         </div>
-        <ChevronRightIcon className="relative h-[10vw] w-[15vw] mx-[3vw] fill-teal self-center" onClick={handleNextSlide}/>
+        <ChevronRightIcon className="relative h-[10vw] w-[15vw] fill-teal self-center" onClick={handleNextSlide}/>
       </section>
     </section>
   );
