48d970454e6a59e89d31216d577735a236379a07
JeffMuter <muterjeffery@gmail.com>
2024-05-13 19:21:47 -0400

    reverting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import Blog from "./pages/Blog";
 import "tailwindcss/tailwind.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
+        <Route path="/" element={<Home />} />      </Routes>
     </Router>
   );
 }
